2524a8b26e02f43da9dd0852d0ff0a07565df236
kalijune <meforus2@gmail.com>
2021-08-13 13:47:39 +0530

    Database Fixed

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -3,9 +3,10 @@ var db=require('../config/connection')
 
 module.exports={
     addProduct:(product,callback)=>{
-        console.log(product);
+        
         db.get().collection('product').insertOne(product).then((data)=>{
-            callback(true)
+            //ops replaced with insertedID
+            callback(data.insertedId)
         })
     }
 }
