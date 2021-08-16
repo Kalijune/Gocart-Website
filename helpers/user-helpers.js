77b0c3b5afc9ddb93c872c586cd3fda96bbc49ff
kalijune <meforus2@gmail.com>
2021-08-16 11:34:16 +0530

    Login from database Succses

--- helpers/user-helpers.js
+++ helpers/user-helpers.js
@@ -11,5 +11,23 @@ module.exports={
         })
         
       
+    },
+    doLogin:(userData)=>{
+        return new Promise(async(resolve,reject)=>{
+            let loginStatus=false
+            let response={}
+            let user=await db.get().collection(collection.USER_COLLEECTION).findOne({Email:userData.Email})
+            if(user){
+                bcrypt.compare(userData.Password,user.Password).then((status)=>{
+                    if(status){
+                        console.log("Login succ");
+                    }else{
+                        console.log("Login Fail");
+                    }
+                })
+            }else{
+                console.log("login failed");
+            }
+        })
     }
 }
